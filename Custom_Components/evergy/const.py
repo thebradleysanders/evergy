31af34246ce3588ea460b9b7a021de6347e511a7
Brad Sanders <thebradleysanders@users.noreply.github.com>
2023-03-24 09:39:39 -0500

    Update const.py

--- Custom_Components/evergy/const.py
+++ Custom_Components/evergy/const.py
@@ -2,11 +2,9 @@
 
 DOMAIN = "evergy"
 
-CONF_USERNAME = "username"
-CONF_PASSWORD = "password"
-
 CONF_NOT_FIRST_RUN = "not_first_run"
 
 FIRST_RUN = "first_run"
 EVERGY_OBJECT = "evergy_object"
 UNDO_UPDATE_LISTENER = "update_update_listener"
+ICON = 'mdi:transmission-tower'

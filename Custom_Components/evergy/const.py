2ae9c3ed0782a213740ecccd92ee4aae23827e4d
Brad Sanders <thebradleysanders@users.noreply.github.com>
2023-03-24 09:14:10 -0500

    Create const.py

---
+++ Custom_Components/evergy/const.py
@@ -0,0 +1,12 @@
+"""Constants for the Evergy online utilities component."""
+
+DOMAIN = "evergy"
+
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+
+CONF_NOT_FIRST_RUN = "not_first_run"
+
+FIRST_RUN = "first_run"
+EVERGY_OBJECT = "evergy_object"
+UNDO_UPDATE_LISTENER = "update_update_listener"

78f1d756e85ecf025220f1f06737f47169027dca
Brad Sanders <thebradleysanders@users.noreply.github.com>
2023-03-25 21:17:47 -0500

    Fix sensor updates add coordinator

--- custom_components/evergy/sensor.py
+++ custom_components/evergy/sensor.py
@@ -2,6 +2,7 @@
 from code import interact
 from datetime import timedelta
 import logging
+import async_timeout
 
 from homeassistant import core
 try:
@@ -17,6 +18,8 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv, entity_platform, service
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.core import callback
+from homeassistant.exceptions import ConfigEntryAuthFailed
 from homeassistant.helpers.update_coordinator import (
     CoordinatorEntity,
     DataUpdateCoordinator,
@@ -26,12 +29,10 @@ from homeassistant.helpers.update_coordinator import (
 from .const import (
     ICON,
     DOMAIN,
-    FIRST_RUN,
     EVERGY_OBJECT
 )
 
 _LOGGER = logging.getLogger(__name__)
-SCAN_INTERVAL = timedelta(seconds=60)
 PARALLEL_UPDATES = 1
 
 async def async_setup_entry(
@@ -40,31 +41,39 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up the Evergy platform."""
-    username = config_entry.data[CONF_USERNAME]
-    evergy = hass.data[DOMAIN][config_entry.entry_id][EVERGY_OBJECT]
-    coordinator = MyCoordinator(hass, evergy)
-
-    entities = [] 
-    _LOGGER.info("Adding sensor entities for Evergy account %s", username)
-    entities.append(EvergySensor(coordinator, "period", config_entry.entry_id, "Period", "mdi:clipboard-text-clock-outline", None))
-    entities.append(EvergySensor(coordinator, "billDate", config_entry.entry_id, "Total Bill Date", "mdi:calendar-range", None))
-    entities.append(EvergySensor(coordinator, "usage", config_entry.entry_id, "Usage Today", "mdi:transmission-tower", "kWh"))
-    entities.append(EvergySensor(coordinator, "demand", config_entry.entry_id, "Demand", "mdi:transmission-tower", "kWh"))
-    entities.append(EvergySensor(coordinator, "avgDemand", config_entry.entry_id, "Average Demand", "mdi:transmission-tower", "kWh"))
-    entities.append(EvergySensor(coordinator, "peakDemand", config_entry.entry_id, "Peak Demand", "mdi:transmission-tower", "kWh"))
-    entities.append(EvergySensor(coordinator, "peakDateTime", config_entry.entry_id, "Peak Time", "mdi:calendar-range", None))
-    entities.append(EvergySensor(coordinator, "maxTemp", config_entry.entry_id, "Max Temp", "mdi:thermometer-high", "°F"))
-    entities.append(EvergySensor(coordinator, "minTemp", config_entry.entry_id, "Min Temp", "mdi:thermometer-low", "°F"))
-    entities.append(EvergySensor(coordinator, "avgTemp", config_entry.entry_id, "Average Temp", "mdi:thermometer-auto", "°F"))
-    entities.append(EvergySensor(coordinator, "cost", config_entry.entry_id, "Cost Today", "mdi:currency-usd", None))
-    entities.append(EvergySensor(coordinator, "balance", config_entry.entry_id, "Balance", "mdi:currency-usd", None))
     
-    entities.append(EvergySensor(coordinator, "address", config_entry.entry_id, "Address", "mdi:home", None))
-    entities.append(EvergySensor(coordinator, "billAmount", config_entry.entry_id, "Bill Amount", "mdi:currency-usd", None))
-    entities.append(EvergySensor(coordinator, "isPastDue", config_entry.entry_id, "Is Past Due", "mdi:calendar-range", None))
-    entities.append(EvergySensor(coordinator, "test", config_entry.entry_id, "DevTest", "mdi:calendar-range", None))
+    async def async_update_data():
+        evergy_api = hass.data[DOMAIN][config_entry.entry_id][EVERGY_OBJECT]
+        await hass.async_add_executor_job(evergy_api.get_usage)
 
-    await coordinator.async_config_entry_first_refresh()
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name="sensor",
+        update_method=async_update_data,
+        update_interval=timedelta(seconds=5400), #90 min.
+    )
+
+    # Immediate refresh
+    await coordinator.async_request_refresh()
+
+    _LOGGER.info("Adding sensor entities for Evergy account %s", config_entry.data[CONF_USERNAME])
+    entities = [] 
+    entities.append(EvergySensor(coordinator, hass, "period", config_entry.entry_id, "Period", "mdi:clipboard-text-clock-outline", None))
+    entities.append(EvergySensor(coordinator, hass, "billDate", config_entry.entry_id, "Total Bill Date", "mdi:calendar-range", None))
+    entities.append(EvergySensor(coordinator, hass, "usage", config_entry.entry_id, "Usage Today", "mdi:transmission-tower", "kWh"))
+    entities.append(EvergySensor(coordinator, hass, "demand", config_entry.entry_id, "Demand", "mdi:transmission-tower", "kWh"))
+    entities.append(EvergySensor(coordinator, hass, "avgDemand", config_entry.entry_id, "Average Demand", "mdi:transmission-tower", "kWh"))
+    entities.append(EvergySensor(coordinator, hass, "peakDemand", config_entry.entry_id, "Peak Demand", "mdi:transmission-tower", "kWh"))
+    entities.append(EvergySensor(coordinator, hass, "peakDateTime", config_entry.entry_id, "Peak Time", "mdi:calendar-range", None))
+    entities.append(EvergySensor(coordinator, hass, "maxTemp", config_entry.entry_id, "Max Temp", "mdi:thermometer-high", "°F"))
+    entities.append(EvergySensor(coordinator, hass, "minTemp", config_entry.entry_id, "Min Temp", "mdi:thermometer-low", "°F"))
+    entities.append(EvergySensor(coordinator, hass, "avgTemp", config_entry.entry_id, "Average Temp", "mdi:thermometer-auto", "°F"))
+    entities.append(EvergySensor(coordinator, hass, "cost", config_entry.entry_id, "Cost Today", "mdi:currency-usd", None))
+    entities.append(EvergySensor(coordinator, hass, "balance", config_entry.entry_id, "Balance", "mdi:currency-usd", None))
+    entities.append(EvergySensor(coordinator, hass, "address", config_entry.entry_id, "Address", "mdi:home", None))
+    entities.append(EvergySensor(coordinator, hass, "billAmount", config_entry.entry_id, "Bill Amount", "mdi:currency-usd", None))
+    entities.append(EvergySensor(coordinator, hass, "isPastDue", config_entry.entry_id, "Is Past Due", "mdi:calendar-range", None))
     async_add_entities(entities, True)
 
     platform = entity_platform.async_get_current_platform()
@@ -77,49 +86,11 @@ async def async_setup_entry(
         if not entities:
             return
 
-        
-      
-class MyCoordinator(DataUpdateCoordinator):
-    """My custom coordinator."""
 
-    def __init__(self, hass, evergy_api):
-        """Initialize my coordinator."""
-        super().__init__(
-            hass,
-            _LOGGER,
-            # Name of the data. For logging purposes.
-            name="My sensor",
-            # Polling interval. Will only be polled if there are subscribers.
-            update_interval=timedelta(seconds=30),
-        )
-        self.evergy_api = evergy_api
-
-    async def _async_update_data(self):
-        """Fetch data from API endpoint.
-
-        This is the place to pre-process the data to lookup tables
-        so entities can quickly look up their data.
-        """
-        try:
-            # Note: asyncio.TimeoutError and aiohttp.ClientError are already
-            # handled by the data update coordinator.
-            async with async_timeout.timeout(10):
-                return await self.evergy_api.get_usage()
-        except ApiAuthError as err:
-            # Raising ConfigEntryAuthFailed will cancel future updates
-            # and start a config flow with SOURCE_REAUTH (async_step_reauth)
-            raise ConfigEntryAuthFailed from err
-        except ApiError as err:
-            raise UpdateFailed(f"Error communicating with API: {err}")
-        
-        
-
-
-class EvergySensor(CoordinatorEntity, SensorEntity):
-    def __init__(self, coordinator, sensor_type: str, namespace, nicename: str, icon: str, uom: str) -> None:
-        """Pass coordinator to CoordinatorEntity."""
-        super().__init__(coordinator)
-
+class EvergySensor(SensorEntity):
+    def __init__(self, coordinator, hass, sensor_type: str, namespace, nicename: str, icon: str, uom: str) -> None:
+        self._coordinator = coordinator
+        self._evergy_api = hass.data[DOMAIN][namespace][EVERGY_OBJECT]
         self._sensor_type = sensor_type
         self._attr_unique_id = f"{namespace}_{self._sensor_type}"
         self._attr_icon = icon
@@ -127,28 +98,32 @@ class EvergySensor(CoordinatorEntity, SensorEntity):
         self._attr_name = f"{nicename}"
         self._attr_native_value = None
         self._attr_native_unit_of_measurement = uom
+        self._attr_should_poll = False
         self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, str(self._coordinator.data['dashboard']['addresses'][0]['street']))},
+            identifiers={(DOMAIN, str(self._evergy_api.dashboard_data['addresses'][0]['street']))},
             manufacturer="Evergy",
             model="Evergy.com Utility Account",
-            name=str(self._coordinator.data['dashboard']['addresses'][0]['street'])
+            name=str(self._evergy_api.dashboard_data['addresses'][0]['street'])
         )
-   
 
-    @callback
-    def _handle_coordinator_update(self) -> None:
-        """Handle updated data from the coordinator."""
+    @property
+    def native_value(self):
         if self._sensor_type == "address":
-            self._attr_native_value = str(self.coordinator.data['dashboard']['addresses'][0]['street'])
+            return str(self._evergy_api.dashboard_data['addresses'][0]['street'])
         elif(self._sensor_type == "billAmount" or self._sensor_type == "isPastDue"):
-            self._attr_native_value = str(self.coordinator.data['dashboard'][self._sensor_type])
+            return str(self._evergy_api.dashboard_data[self._sensor_type])
         else:
-            self._attr_native_value = str(self.coordinator.data['usage'][-1][self._sensor_type])
-            
-        self.async_write_ha_state()
-
+            return str(self._evergy_api.usage_data[-1][self._sensor_type])
 
     @property
     def entity_registry_enabled_default(self):
         """Return if the entity should be enabled when first added to the entity registry."""
         return True
+
+    async def async_added_to_hass(self):
+        """When entity is added to hass."""
+        self.async_on_remove(
+            self._coordinator.async_add_listener(
+                self.async_write_ha_state
+            )
+        )
